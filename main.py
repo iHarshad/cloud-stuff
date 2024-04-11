d20594ade506afe2b3f9dc94e5e5f7818163bae4
Harshad <iHarshad@users.noreply.github.com>
2024-04-11 12:48:29 +0530

    update main code

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
-from datetime import datetime
+import datetime as dt
+import os
 
-print(f"Hello {datetime.now()}")
+import sqlalchemy as sa
+from supabase import Client, create_client
+from supabase.client import ClientOptions
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_URL: str = os.environ.get("SUPABASE_URL")
+DB_KEY: str = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(
+    DB_URL,
+    DB_KEY,
+    options=ClientOptions(postgrest_client_timeout=10, storage_client_timeout=10),
+)
+
+
+print(f"Hello {dt.datetime.now()}")
+
+print(f"GITHUB_RUN_ID = {os.environ.get('GITHUB_RUN_ID')}")
+print(f"GITHUB_SHA: {os.environ.get('GITHUB_SHA')}")
+print(f"GITHUB_EVENT_NAME: {os.environ.get('GITHUB_EVENT_NAME')}")
+print(f"GITHUB_JOB: {os.environ.get('GITHUB_JOB')}")
+
+print(f"Message: { os.environ.get('GITHUB_EVENT_INPUTS_MESSAGE1') }")
+print(f"Tags: {os.environ.get('INPUT_MESSAGE')}"
